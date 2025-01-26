3a9178b52f5bd0c675b59bf1bb5cb1d769302c49
samir rain <124909580+codewithsamir@users.noreply.github.com>
2025-01-26 09:33:03 +0000

    second

--- database/dbconnect.js
+++ database/dbconnect.js
@@ -32,13 +32,52 @@ class DatabaseConnection {
   }
 
   async  connect() {
-    if(!process.env.MONGO_URL){
-        throw new Error("Mongo db URI is not  defined in env variables")
+   try {
+     if(!process.env.MONGO_URL){
+         throw new Error("Mongo db URI is not  defined in env variables")
+     }
+     const  connectionOptions = {
+         useNewUrlParser:true,
+         useUnifiedTopology:true,
+         maxPoolSize :10,
+         serverSelectionTimeoutMS : 5000,
+         socketTimeoutMS: 45000,
+         family: 4 // use Ipv4
+     };
+ 
+     if(process.env.NODE_ENV === 'development'){
+       mongoose.set('debug',true)
+     }
+ 
+     await mongoose.connect(process.env.MONGO_URL, connectionOptions);
+ 
+     this.retryCount = 0 //reset retry count on success
+   } catch (error) {
+    console.error(error.message)
+    await this.handleConnectionError();
+   }
+
+    
+  }
+
+  async handleConnectionError(){
+    if(this.retryCount < MAX_RETRIES){
+      this.retryCount++;
+      console.log(`Retrying connection.... Attemp ${this.retryCount} of ${MAX_RETRIES}`);
+      await new Promise(resolve =>setTimeout(() => {
+        resolve()
+      }, RETRY_INTERVAL))
+      return this.connect()
+    }else{
+      console.error(`Failed to connect to MONGODB after ${MAX_RETRIES}`)
+      process.exit(1)
     }
-    const  connectionOptions = {
-        useNewUrlparser:true,
-        useUnifiedTopology:true,
-        maxPoolSize :10,
+  }
+
+  async handleDisconnection(){
+    if(!this.isConnected){
+      console.log("Attempting to reconnected to mongodb...")
+      this.connect(  )
     }
   }
 }
